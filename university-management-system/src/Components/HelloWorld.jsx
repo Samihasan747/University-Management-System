8063afca6ca09e4db6b9c2d2aad2c43879f7c441
Samihasan747 <1102samihasan@gmail.com>
2026-02-26 02:08:25 +0600

    Tailwind Connected

---
+++ university-management-system/src/Components/HelloWorld.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function HelloWorld() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div class="m-auto w-64">
+        <b>Hello World,</b>
+        <br/>
+        <br/>
+        <span className="text-centre">
+          This Project is University Management System Web Application built by{" "}
+          <i>Sami Hasan</i> and <i>Sadiqun Nabi</i>.
+        </span>
+      </div>
+    </div>
+  );
+}
